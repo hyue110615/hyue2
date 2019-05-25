8367485177aa0d1fe4207f51beacae0b315af38e
hyue110615 <hyue110615@126.com>
2019-05-25 09:23:22 +0800

    hyue2

---
+++ hyue2/src/com/hyue/test2/test.java
@@ -0,0 +1,7 @@
+package com.hyue.test2;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("hyue is big cool boy");
+	}
+}
---
+++ hyue2/src/com/hyue/test3/test2.java
@@ -0,0 +1,12 @@
+package com.hyue.test3;
+
+import org.junit.Test;
+
+public class test2 {
+	
+	@Test
+	public void test() {
+		System.out.println("11111111111111111");
+	}
+
+}
